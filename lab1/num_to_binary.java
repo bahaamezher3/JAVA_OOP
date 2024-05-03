a630cd25e1351a6195aa1e6f3ae36ab5d78a86c6
3mklbahaa <113281341+3mklbahaa@users.noreply.github.com>
2024-05-03 21:36:05 +0300

    Create num_to_binary.java

---
+++ lab1/num_to_binary.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n;
+    System.out.println("plz enter n: ");
+    n = sc.nextInt();
+    String s = "";
+    while (n > 0) {
+      s = n % 2 + s;
+      n = n / 2;
+    }
+    System.out.println("s = " + s);
+  }
+  }
